cf552fe289a1b00dd0da0348a39b85e3f2e92702
Aibek Minbaev <71759007+AibekMinbaev@users.noreply.github.com>
2023-03-13 22:06:37 +0900

    Added cities field in Properties model

--- listings/forms.py
+++ listings/forms.py
@@ -1,10 +1,12 @@
 from django.forms import ModelForm 
 from django import forms
-from .models import Listing, state_choices
+from .models import Listing 
+from localflavor.us.us_states import STATE_CHOICES
 
 
 class ListingForm(ModelForm): 
-    state = forms.ChoiceField(choices= state_choices, required=True)
+    state = forms.ChoiceField(choices= STATE_CHOICES, required=True)
+    city = forms.ChoiceField(choices=[]) 
     class Meta: 
         model = Listing 
         fields = ["title", "price","state","num_bedrooms", "num_bathrooms", "square_footage", "address", "property_image"] 
--- listings/models.py
+++ listings/models.py
@@ -3,60 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
-state_choices = [
-    ('AL', 'Alabama'),
-    ('AK', 'Alaska'),
-    ('AZ', 'Arizona'),
-    ('AR', 'Arkansas'),
-    ('CA', 'California'),
-    ('CO', 'Colorado'),
-    ('CT', 'Connecticut'),
-    ('DE', 'Delaware'),
-    ('FL', 'Florida'),
-    ('GA', 'Georgia'),
-    ('HI', 'Hawaii'),
-    ('ID', 'Idaho'),
-    ('IL', 'Illinois'),
-    ('IN', 'Indiana'),
-    ('IA', 'Iowa'),
-    ('KS', 'Kansas'),
-    ('KY', 'Kentucky'),
-    ('LA', 'Louisiana'),
-    ('ME', 'Maine'),
-    ('MD', 'Maryland'),
-    ('MA', 'Massachusetts'),
-    ('MI', 'Michigan'),
-    ('MN', 'Minnesota'),
-    ('MS', 'Mississippi'),
-    ('MO', 'Missouri'),
-    ('MT', 'Montana'),
-    ('NE', 'Nebraska'),
-    ('NV', 'Nevada'),
-    ('NH', 'New Hampshire'),
-    ('NJ', 'New Jersey'),
-    ('NM', 'New Mexico'),
-    ('NY', 'New York'),
-    ('NC', 'North Carolina'),
-    ('ND', 'North Dakota'),
-    ('OH', 'Ohio'),
-    ('OK', 'Oklahoma'),
-    ('OR', 'Oregon'),
-    ('PA', 'Pennsylvania'),
-    ('RI', 'Rhode Island'),
-    ('SC', 'South Carolina'),
-    ('SD', 'South Dakota'),
-    ('TN', 'Tennessee'),
-    ('TX', 'Texas'),
-    ('UT', 'Utah'),
-    ('VT', 'Vermont'),
-    ('VA', 'Virginia'),
-    ('WA', 'Washington'),
-    ('WV', 'West Virginia'),
-    ('WI', 'Wisconsin'),
-    ('WY', 'Wyoming'),
-]
-
-
 class Listing(models.Model): 
     user = models.ForeignKey(User,blank=True, null=True, on_delete=models.CASCADE)
     title = models.CharField(max_length=150) 
@@ -64,11 +10,12 @@ class Listing(models.Model):
     num_bedrooms = models.IntegerField() 
     num_bathrooms = models.IntegerField()
     square_footage = models.IntegerField()
-    state = models.CharField(max_length=2, blank=True,null=True) 
+    state = models.CharField(max_length=2, blank=True,null=True)
+    city = models.CharField(max_length=120, blank=True, null=True) 
+    street = models.CharField(max_length=120, blank=True, null=True)  
     address = models.CharField(max_length=120) # more clear address with real streets home and address 
     property_image = models.ImageField(default = 'defaultproperty.jpg', blank=True, null=True) # adding multiple images
     
-
     def __str__(self): 
         return self.title 
 
